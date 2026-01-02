03fcc20a8d647a180b8a962390709ce25596e924
Alex <pav.perm.17@yandex.ru>
2026-01-02 04:01:15 +0300

    fix: build issues 3
    Former-commit-id: 82d5157d55a1293ea2b6e104c9c949194b55397a

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    cacheComponents: true,
     typescript: {
         ignoreBuildErrors: true,
     },
@@ -22,7 +15,7 @@ const nextConfig = {
     },
     turbopack: {
         resolveAlias: {
-            '@contracts': path.resolve(__dirname, '../contracts'),
+            '@contracts': './contracts',
         },
     },
     async rewrites() {
