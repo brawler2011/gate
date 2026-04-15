a496d6d84243b927018cac9e0cb93ecc9ece8424
brawler2011 <slavabirin1@yandex.ru>
2026-04-15 20:34:51 +0300

    refactor: move constants to runtime.go

--- backend/runtime.go
+++ backend/runtime.go
@@ -37,6 +37,14 @@ import (
 const submissionsRingBufferSize = 10000
 const serviceShutdownTimeout = 5 * time.Second
 
+const (
+	defaultS3Region         = "us-east-1"
+	defaultS3AvatarBucket   = "avatars"
+	defaultS3PackageBucket  = "problem-packages"
+	defaultS3WorkshopBucket = "problem-workspaces"
+	defaultS3BlogBucket     = "blog-images"
+)
+
 type appService interface {
 	Name() string
 	Start(ctx context.Context) error
--- backend/s3_defaults.go
+++
@@ -1,9 +0,0 @@
-package main
-
-const (
-	defaultS3Region         = "us-east-1"
-	defaultS3AvatarBucket   = "avatars"
-	defaultS3PackageBucket  = "problem-packages"
-	defaultS3WorkshopBucket = "problem-workspaces"
-	defaultS3BlogBucket     = "blog-images"
-)
