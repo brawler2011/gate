c3f725bf852f2f197c85e00d09dbea286d3a141c
brawler2011 <slavabirin1@yandex.ru>
2026-08-06 00:01:18 +0500

    impr: proxying

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,19 +2,6 @@
 const nextConfig = {
     output: 'standalone',
     pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    experimental: {
-        serverActions: {
-            bodySizeLimit: '50mb',
-        },
-    },
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: `${process.env.BACKEND_API_URL || 'http://localhost:8080'}/:path*`,
-            },
-        ];
-    },
 }
 
 export default nextConfig
